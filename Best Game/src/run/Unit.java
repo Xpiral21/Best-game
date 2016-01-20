1b64b4e6e83ce53de44f78a36f88f5e2545851c4
Xpiral21 <adry_is@yahoo.com>
2016-01-20 10:36:01 +0200

    clasa unit

---
+++ Best Game/src/run/Unit.java
@@ -0,0 +1,50 @@
+package run;
+
+public class Unit {
+	private double hp;
+	private double maxDamage;
+	private double minDamage;
+	private double defence;
+	private double mana;
+
+	public double getHp() {
+		return hp;
+	}
+
+	public void setHp(double hp) {
+		this.hp = hp;
+	}
+
+	public double getMaxDamage() {
+		return maxDamage;
+	}
+
+	public void setMaxDamage(double maxDamage) {
+		this.maxDamage = maxDamage;
+	}
+
+	public double getMinDamage() {
+		return minDamage;
+	}
+
+	public void setMinDamage(double minDamage) {
+		this.minDamage = minDamage;
+	}
+
+	public double getDefence() {
+		return defence;
+	}
+
+	public void setDefence(double defence) {
+		this.defence = defence;
+	}
+
+	public double getMana() {
+		return mana;
+	}
+
+	public void setMana(double mana) {
+		this.mana = mana;
+	}
+
+}
