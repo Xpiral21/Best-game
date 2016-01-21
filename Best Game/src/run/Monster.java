d9002736f63da6eacce2068f5b4da5726395f6fb
Xpiral21 <adry_is@yahoo.com>
2016-01-21 14:06:56 +0200

    int

--- Best Game/src/run/Monster.java
+++ Best Game/src/run/Monster.java
@@ -1,7 +1,7 @@
 package run;
 
 public class Monster extends Unit {
-	public Monster(double hp, double maxDamage, double minDamage, double defense, double mana, String name){
+	public Monster(int hp, int maxDamage, int minDamage, int defense, int mana, String name){
 		super(hp, maxDamage, minDamage,defense,mana, name);
 		setDescription("The "+getName()+" does between "+getMinDamage()+"-"+getMaxDamage()+" and has "+getHp()+" HP");
 	}
