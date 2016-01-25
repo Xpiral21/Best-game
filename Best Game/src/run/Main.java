c84cb68d9850baa5fea39dd8831cab39337bd355
Xpiral21 <adry_is@yahoo.com>
2016-01-25 10:39:05 +0200

    muzica

--- Best Game/src/run/Main.java
+++ Best Game/src/run/Main.java
@@ -4,6 +4,7 @@ package run;
 public class Main {
 
 	public static void main(String[] args) {
+SimplePlayer player=new SimplePlayer();
 
 	
 
---
+++ Best Game/src/run/SimplePlayer.java
@@ -0,0 +1,16 @@
+package run;
+import javazoom.jl.player.*;
+import java.io.FileInputStream;
+
+public class SimplePlayer {
+	public SimplePlayer(){
+	try{
+
+	    FileInputStream fis = new FileInputStream("lib/MelodiaEpica.mp3");
+	    Player playMP3 = new Player(fis);
+
+	    playMP3.play();
+
+	    }catch(Exception e){System.out.println(e);}
+}
+}
