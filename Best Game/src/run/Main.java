dc2921a196b226c8219ae4f6b2797dc088815d0d
Xpiral21 <adry_is@yahoo.com>
2016-01-25 09:51:21 +0200

    a

--- Best Game/src/run/Main.java
+++ Best Game/src/run/Main.java
@@ -3,11 +3,10 @@ package run;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int k = 10;
-		int q=10;
-		System.out.println(k);
-		System.out.println(q);
+		
+		
+		
+	
 	}
 
 }
