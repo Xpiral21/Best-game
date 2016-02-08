91c650d1637b4d9e8d707ff200976860f72cdb9e
Xpiral21 <adry_is@yahoo.com>
2016-02-08 19:15:02 +0200

    modificare UnitCreator

--- Best Game/src/run/UnitCreator.java
+++ Best Game/src/run/UnitCreator.java
@@ -1,12 +1,11 @@
 package run;
 public class UnitCreator {
 
-	public Unit getUnit(String unitType) {
+	public Hero getHero(String unitType) {
 
 		if (unitType == "Warrior")
 			return new Hero("Thor", "God of Thunder", 1000, 1000, 25, 75, 20, 10,10);
-		else if (unitType == "Monster")
-			return new Monster("Loki", "Lord of Jotunheim", 800, 800, 50, 75, 20, 10,10);
+		
 		else
 			return null;
 
