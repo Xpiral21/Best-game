f7361442c389aaaf9d2910adcab8dd0c44f42022
Alex <puscasu903@yahoo.com>
2016-06-29 16:38:01 +0300

    heal

--- Best Game/src/run/Trade.java
+++ Best Game/src/run/Trade.java
@@ -3,9 +3,10 @@ package run;
 public class Trade {
 	public Trade(Hero h, Trader t) {
 		System.out.println();
-		System.out.println("You have encountered a Trader , you can buy items by typing their number.");
+		System.out.println(
+				"You have encountered a Trader , you can buy items by typing their number,or heal by pressing 'h'");
 		System.out.println("Type a number higher than the highest item number to Exit the shop");
-		System.out.println("Your current gold is : " +h.getGold());
+		System.out.println("Your current gold is : " + h.getGold());
 		System.out.println();
 		System.out.println(t.inventory);
 		int itemNumber = 0;
@@ -18,8 +19,18 @@ public class Trade {
 			} catch (InterruptedException e1) {
 
 			}
-			itemNumber = Integer.parseInt(GUI.getStdin());
-			if (itemNumber >= 0 && itemNumber < t.inventory.getSize()) {
+			String userInput = GUI.getStdin();
+			if (!userInput.contains("h")) {
+				itemNumber = Integer.parseInt(userInput);
+			}
+			if (userInput.contains("h")) {
+				if (h.getGold() >= 50) {
+					h.setGold(h.getGold() - 50);
+					h.setCurrentHitPoints(h.getMaxHitPoints());
+				}
+				else
+					System.out.println("Sucks to be you!");
+			} else if (itemNumber >= 0 && itemNumber < t.inventory.getSize()) {
 				if (h.getGold() >= t.inventory.getItem(itemNumber).getValue()) {
 					t.buyItem(itemNumber, h);
 					h.equipItem(h.inventory.getItem(h.inventory.getSize() - 1));
